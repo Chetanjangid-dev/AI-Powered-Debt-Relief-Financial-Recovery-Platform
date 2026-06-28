b061b734eda907474e1190590ded7d0e5ca6ca09
Chetali jaiswal <jaiswalchetali2507@gmail.com>
2026-06-28 12:48:01 +0530

    frontend-setup-done

---
+++ Frontend/vite-project/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+// import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <h2>AI Powered Debt Relief & Financial Recovery Platform</h2>
+    </>
+  )
+}
+
+export default App
