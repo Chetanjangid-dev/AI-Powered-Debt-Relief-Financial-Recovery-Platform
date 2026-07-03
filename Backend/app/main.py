4b4bcca78b72db829cadac8829c2829d9a591f49
Chetan jangid <jangidchetan.jpr@gmail.com>
2026-07-03 14:21:16 +0530

    Update main.py

--- Backend/app/main.py
+++ Backend/app/main.py
@@ -26,7 +26,9 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173",
         "http://localhost:3000",
+        "https://ai-powered-debt-relief-financial-recovery-platform.vercel.app",
         "https://ai-powered-debt-relief-financial-recovery-platform-ca6a2fnj6.vercel.app",
+        "https://ai-powered-debt-relief-fi-git-dff996-chetanjangid-devs-projects.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
